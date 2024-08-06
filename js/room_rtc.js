a2424854cc8b759f327505295f4d0e0fd77074b2
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-08-06 22:59:34 +0700

    Publishing streams & handling remote users

--- js/room_rtc.js
+++ js/room_rtc.js
@@ -23,6 +23,9 @@ let remoteUsers = {}
 let joinRoomInit = async () => {
     client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
     await client.join(APP_ID, roomId, token, uid)
+
+    client.on('user-published', handleUserPublished)
+    client.on('user-left', handleUserLeft)
     joinStream()
 }
 
@@ -37,6 +40,32 @@ let joinStream = async () => {
     document.getElementById('streams__container').insertAdjacentHTML('beforeend', player) 
 
     localTracks[1].play(`user-${uid}`)
+    await client.publish([localTracks[0], localTracks[1]])
+}
+
+let handleUserPublished = async (user, mediaType) => {
+    remoteUsers[user.uid] = user
+
+    await client.subscribe(user, mediaType)
+
+    let player = document.getElementById(`user-container-${user.uid}`)
+    if(player === null){
+        player = `<div class="video__container" id="user-container-${user.uid}">
+                        <div class="video-player" id="user-${user.uid}"></div>
+                    </div>`
+        document.getElementById('streams__container').insertAdjacentHTML('beforeend', player)
+    }
+    if(mediaType == 'video'){
+        user.videoTrack.play(`user-${user.uid}`)
+    }
+    if(mediaType == 'audio'){
+        user.audioTrack.play()
+    }
+}
+
+let handleUserLeft = async (user) => {
+    delete remoteUsers[user.uid]
+    document.getElementById(`user-container-${user.uid}`).remove()
 }
 
 joinRoomInit()
