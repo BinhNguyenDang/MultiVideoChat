bbb591027508f0308f1c452e2bb00d07cb980b9f
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-08-07 15:59:52 +0700

    Creating rooms

--- js/room_rtc.js
+++ js/room_rtc.js
@@ -17,6 +17,10 @@ if(!roomId){
     roomId = 'main'
 }
 
+let displayName = sessionStorage.getItem('display_name')
+if(!displayName){
+    window.location = 'lobby.html'
+}
 let localTracks = []
 let remoteUsers = {}
 
